48546444ff63ece698760604d66ebe86b115ffe5
Dreamica <460985640@qq.com>
2021-08-18 11:51:18 +0800

    Update zh.js

--- docs/.vuepress/sidebar/zh.js
+++ docs/.vuepress/sidebar/zh.js
@@ -325,9 +325,9 @@ module.exports = {
                     title: '使用指南',
                     collapsable: true,
                     children: [
-                        ['msp/guides/platform/msp-project', '创建微服务项目'],
-                        ['msp/guides/platform/msp-project-list', '平台总览'],
-                        ['msp/guides/platform/msp-overview', '项目列表'],
+                        ['msp/guides/platform/msp-project', '创建项目'],
+                        ['msp/guides/platform/msp-overview', '平台总览'],
+                        ['msp/guides/platform/msp-project-list', '项目列表'],
                         'msp/guides/apm/topology',
                         // {
                         //     title: '微服务环境',
